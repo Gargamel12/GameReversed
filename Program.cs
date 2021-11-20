413dd8e3140e6e9629e954a5be8cd6f36ff60f5d
Gargamel12 <Janecek.Ales@stud.vassboskovice.cz>
2021-11-20 17:00:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameReversed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
